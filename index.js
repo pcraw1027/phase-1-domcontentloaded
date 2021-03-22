ee9eeacee488606acbd533584568e0db84fe74d5
petercrawford <pcraw1027@gmail.com>
2021-03-22 09:53:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let newMeassage = document.querySelector("p#text");
+    newMeassage.innerHTML = "This is really cool!";
+    // console.log("The DOM has loaded");
+  }); 
+
+  
